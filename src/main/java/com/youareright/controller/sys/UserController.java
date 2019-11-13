5f85e3f953e63735f3948cf934d9e7412d7eb43a
Cancerhzc <919993145@qq.com>
2019-11-13 16:43:36 +0800

    20191113  goods

---
+++ src/main/java/com/youareright/controller/sys/UserController.java
@@ -0,0 +1,94 @@
+package com.youareright.controller.sys;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.youareright.model.sys.PageResult;
+import com.youareright.model.sys.UserEntity;
+import com.youareright.service.sys.UserService;
+
+@RestController
+/*@PreAuthorize("hasRole('ADMI')")*/
+public class UserController {
+
+	private Logger log = LoggerFactory.getLogger(UserController.class);
+
+	@Resource(name = "userServiceImpl")
+	private UserService userService;
+
+	@GetMapping("/user/{loginName}")
+	public UserEntity userGet(@PathVariable String loginName) {
+		UserEntity userEntity = userService.getUserEntityByLoginName(loginName);
+		log.debug("The method is ending");
+		return userEntity;
+	}
+
+	/**
+	 * 获取user表数据
+	 * 
+	 * @param loginName
+	 * @param pageSize
+	 * @param page
+	 * @return
+	 */
+	@GetMapping("/users")
+	public PageResult usersList(String loginName, int pageSize, int page) {
+		PageResult pageResult = new PageResult();
+		pageResult.setData(userService.usersList(loginName, pageSize, page * pageSize));
+		pageResult.setTotalCount(userService.usersSize(loginName, pageSize, page * pageSize));
+		log.debug("The method is ending");
+		return pageResult;
+	}
+
+	/**
+	 * 新建用户信息
+	 * 
+	 * @param userEntity
+	 * @return
+	 */
+	@PostMapping("/users/user")
+	public UserEntity insertUser(@RequestBody UserEntity userEntity) {
+		userService.insertUser(userEntity);
+		log.debug("The method is ending");
+		return userEntity;
+	}
+
+	/**
+	 * 更新用户信息
+	 * 
+	 * @param userEntity
+	 * @param id
+	 * @return
+	 */
+	@PutMapping("/users/{id}")
+	public UserEntity updateUser(@RequestBody UserEntity userEntity, @PathVariable int id) {
+		if (userEntity.getId() == id) {
+			userService.updateUser(userEntity);
+		}
+		log.debug("The method is ending");
+		return userEntity;
+	}
+
+	/**
+	 * 删除用户信息
+	 * 
+	 * @param groupId
+	 * @return
+	 */
+	@DeleteMapping("/users")
+	public List<String> deleteUsers(@RequestBody List<String> groupId) {
+		userService.deleteUsers(groupId);
+		return groupId;
+	}
+}
---
+++ src/main/java/com/youareright/service/impl/sys/UserServiceImpl.java
@@ -0,0 +1,70 @@
+package com.youareright.service.impl.sys;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.youareright.dao.UserDao;
+import com.youareright.model.sys.UserEntity;
+import com.youareright.service.sys.UserService;
+
+@Service(value = "userServiceImpl")
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserDao userDao;
+	
+	@Override
+	public void insert(UserEntity userEntity) {
+		userDao.insert(userEntity);
+	}
+
+	@Override
+	public void del(UserEntity userEntity) {
+		userDao.del(userEntity);
+	}
+
+	@Override
+	public UserEntity getUserEntityByLoginName(String loginName) {
+		return userDao.getUserEntityByLoginName(loginName);
+	}
+
+	@Override
+	public List<UserEntity> usersList(String loginName, int pageSize, int start) {
+		return userDao.usersList( loginName,  pageSize,  start);
+	}
+
+	@Override
+	public Integer usersSize(String loginName, int pageSize, int start) {
+		return userDao.usersSize(loginName, pageSize, start);
+	}
+
+	@Override
+	public void insertUser(UserEntity userEntity) {
+		/*String password = null;
+		try {
+			password = MD5Util.encrypt(userEntity.getPassword());
+			userEntity.setPassword(password);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}*/
+		//userEntity.setPassword(new Md5PasswordEncoder().encodePassword(userEntity.getPassword(), null));
+		userEntity.setPassword("{bcrypt}"+new BCryptPasswordEncoder().encode(userEntity.getPassword()));
+		userDao.insertUser(userEntity);
+	}
+
+	@Override
+	public void updateUser(UserEntity userEntity) {
+		//userEntity.setPassword(new Md5PasswordEncoder().encodePassword(userEntity.getPassword(), null));
+		userEntity.setPassword("{bcrypt}"+new BCryptPasswordEncoder().encode(userEntity.getPassword()));
+		userDao.updateUser(userEntity);
+	}
+
+	@Override
+	public void deleteUsers(List<String> groupId) {
+		userDao.deleteUsers(groupId);
+	}
+
+}
